07dc85e0fa2eff6f28e0bb528b05bc9f89cdd240
s.orcun <s.orcun@altrnativ.com>
2022-12-21 14:01:30 +0100

    adding css to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import "./Button.css";
 
 interface ButtonProps {
   label: string;
-  variant: string;
+  variant?: string;
 }
 
 function Button(props: ButtonProps) {
   return <button className={`btn btn-${props.variant}`}>{props.label}</button>;
 }
+
+Button.defaultProps = {
+  variant: "primary",
+};
 
 export default Button;
